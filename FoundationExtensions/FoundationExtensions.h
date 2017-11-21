f0975e4f4ab92b8d2c0b6ea29aeb240c82eee347
Kirill Ushkov <kirill.u@itomy.ch>
2017-11-21 17:05:03 +0200

    Initial Commit

---
+++ FoundationExtensions/FoundationExtensions.h
@@ -0,0 +1,13 @@
+//
+//  FoundationExtensions.h
+//  FoundationExtensions
+//
+//  Created by Kirill Ushkov on 21.11.17.
+//  Copyright © 2017 itomy.ch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FoundationExtensions : NSObject
+
+@end
---
+++ FoundationExtensions/FoundationExtensions.m
@@ -0,0 +1,13 @@
+//
+//  FoundationExtensions.m
+//  FoundationExtensions
+//
+//  Created by Kirill Ushkov on 21.11.17.
+//  Copyright © 2017 itomy.ch. All rights reserved.
+//
+
+#import "FoundationExtensions.h"
+
+@implementation FoundationExtensions
+
+@end
